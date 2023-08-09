fe3293c0e38aed854769c57a1479a400d44f2a52
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-08-09 17:22:54 +0700

    doneeeeeeeeeeeee

---
+++ 03-Function-Expression/_Demo/_demo/main.js
@@ -0,0 +1,63 @@
+// // console.log('anonymous');
+// // name fn, FN declaration
+// // function add () {}
+
+
+// // #2 : Anonymous
+
+// // IIFE : Intermediate Invoke Function Expression
+
+// // console.log(
+// // (function (x, y ) { 
+   
+// // }) (10,7)
+// // // ); 
+
+// // let a =(function (x, y) {
+// //     return x + y;
+// // })(10, 7);
+
+// // console.log(a);
+
+// /* 2B */
+// //variable = Expression
+// let myVar = ((5*2*20) / 2) % 7;
+
+// //variable = FN (FN == Expression)
+
+// // //ASSIGN anonymous FN to varia
+// // const myFunc = function (x, y ) {
+// //     return x + y; 
+// // };
+
+// // console.log(myFunc(5, 10));
+
+// // //assign named FN to varibale 
+// // const a = console.log;
+// // a('hi');
+
+
+
+// //### Anonym (Modern : Arrow syntax)
+
+// let a = function (x) {
+//     return x **2; };
+// let b = (x) => x ** 2;
+
+
+// //A shortest
+// let a = x => x + 2;
+// let b = (x, y ) => x + y;
+
+// //b Bracket
+// let c = (x, y ) => {
+//     let result = x + y;
+//     return result;
+// };
+
+// console.log(a(3));
+// console.log(a(3, 5));
+ 
+
+
+
