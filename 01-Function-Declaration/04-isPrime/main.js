357403e9ac9b743cd43537f7c173d3ca9bc4d7ea
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-08-09 14:11:46 +0700

    Update main.js

--- 01-Function-Declaration/04-isPrime/main.js
+++ 01-Function-Declaration/04-isPrime/main.js
@@ -1,32 +1,75 @@
-/* prime number
-ตัวตั้ง n (fixed)
-ตั้วหาร dynamic => 1 loop */
+// STAR PATTERN
+// dynamics row => 1 for loop
+// dynamics col => 1 for loop
 
-let n = 5;
-let isPrime = true;
+// *
+// **
+// ***
+// ****
 
-for (let divider = 2; divider < n; divider++ ) {
-    if( n % divider == 0) {
-        isPrime = false ;
-        break; 
+// Prime Number
+// ตัวตั้ง n (fixed)
+// ตัวหาร dynamics => 1 LOOP
 
-    }
-}
-if (isPrime) console.log(n); 
+// n = 5 : 2 3 4
+// n = 6 : 2 3 4 5
+// n = 7 : 2 3 4 5 6
 
-/* design fn
-1 input ? => number ที่ใช้เช็ค
-2 logic implement ?  
-3 output ? => boolean true, false 
-number => boolean */
+// let n = 7;
+// let isPrime = true;
+
+// for (let divider = 2; divider < n; divider++) {
+//     if (n % divider == 0) {
+//         isPrime = false;
+//         break;
+//     }
+// }
+// if (isPrime) console.log(n);
+
+// DESIGN FN
+// 1-INPUT ? => number ที่ใช้เช็ค
+// 2-LOGIC IMPLEMENT ?
+// 3-OUTPUT ? => boolean true,false
+// number => bool
+
 
 function checkPrime(num) {
-    let isPrime = true 
-    
+    let isPrime = true;
+    for (let d = 2; d < num; d++) {
+        if (num % d == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) console.log(num);
 }
 
+// checkPrime(7);
+// checkPrime(13);
+// checkPrime(61);
+// checkPrime(63);
+// checkPrime(83);
 
-/* Abstaction 
-simple idea => one idea
-hiding complexity 
-simple interaction */
+function printPrime(limit) {
+    for (let n = 2; n <= limit; n++) {
+        checkPrime(n);
+    }
+}
+printPrime(71);
+// checkPrime(2);
+// checkPrime(3);
+// checkPrime(4);
+// checkPrime(5);
+// checkPrime(6);
+// checkPrime(7);
+// checkPrime(8);
+// checkPrime(9);
+// checkPrime(10);
+// checkPrime(11);
+// checkPrime(12);
+// checkPrime(13);
+
+// ABSTRACTION
+// - simple ideas => one idea
+// - hiding complexity
+// - simple interaction
